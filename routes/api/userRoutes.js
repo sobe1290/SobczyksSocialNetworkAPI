e70db1f7bf8ad63e0fd09ed5e85e9bc9ed313e48
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-10-30 13:45:42 -0700

    starting routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,4 +9,12 @@ router.route('/').get(getUsers).post(createUser);
 
 router.route('/:userId').get(getSingleUser);
 
+//PUT to update a user by its _id
+
+//DELETE to remove user by its _id
+
+///api/users/:userId/friends/:friendId
+//POST to add a new friend to a user's friend list
+//DELETE to remove a friend from a user's friend list
+
 module.exports = router;
